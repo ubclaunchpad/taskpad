b4a6f0394b109dcc3434928459ecbed5ec75ca25
Schemetrical <Schemetrical@icloud.com>
2019-01-18 23:07:41 -0800

    Initial Commit

---
+++ taskpad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskpad
+//
+//  Created by Yichen Cao on 2019-01-18.
+//  Copyright © 2019 UBC Launch Pad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
